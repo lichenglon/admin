aae083137f4e3de040940c950e66b849292aa3dc
lichenglonaa$ <34432269+lichenglon@users.noreply.github.com>
2018-01-11 18:01:40 +0800

    dgfdagf

--- resources/lang/en/house_translate.php
+++ resources/lang/en/house_translate.php
@@ -8,7 +8,7 @@ return [
 	'Add_type'                  => 'Add type',                      //添加类型
 	'folding'                   => 'folding',                       //折叠
 	'The_sorting'               => 'The sorting',                   //排序
-	'nomenclature'              => 'Nomenclature',                  //名称
+	'name'                      => 'Name',                  //名称
 	'delete'                    => 'Delete',                        //删除
 
 	//房源添加
@@ -18,6 +18,7 @@ return [
 	'Detailed_location'         => 'Detailed location',             //详细位置
 	'Housing_structure'         => 'Housing structure',             //房源结构
 	'surrounding_information'   => 'Surrounding information',       //周边信息
+	'Minute'                    => 'Minute',                        //分钟
 	'supermarket'               => 'Supermarket',                   //超市
 	'Chinese_restaurant'        => 'Chinese restaurant',            //中餐馆
 	'The_police_station'        => 'The police station',            //警局
@@ -26,7 +27,10 @@ return [
 	'Housing_size'              => 'Housing size',                  //房源大小
 	'deposit'                   => 'Deposit',                       //押金
 	'Prepayment_ratio'          => 'Prepayment ratio',              //预付款比例
+	'One_lease_and_one_rent'    => 'One lease and one rent',        //一押一租
 	'The_method_of_payment'     => 'The method of payment',         //结算方式
+	'Monthly'                   => 'Monthly',                       //月结
+	'Quarterly_settlement'      => 'Quarterly settlement',          //季结
 	'House_equipment'           => 'House equipment',               //房屋设备
 	'Washing_machine'           => 'Washing machine',               //洗衣机
 	'Air_conditioning'          => 'Air conditioning',              //空调
@@ -38,7 +42,7 @@ return [
 	'The_keyword'               => 'The keyword',                   //关键字
 	'Introduction_of_housing'   => 'Introduction of housing',       //房源简介
 	'The_lease_time'            => 'The lease time',                //租期时长
-	'The lease period'          => 'The lease period',              //起租期
+	'The_lease_period'          => 'The lease period',              //起租期
 	'The_longest_leases'        => 'The longest leases',            //最长租期
 	'Home_state'                => 'Home state',                    //房屋状态
 	'Rent_in_advance'           => 'Rent in advance',               //预租
@@ -56,5 +60,10 @@ return [
 	'boy'                       => 'Boy',                           //男
 	'girl'                      => 'Girl',                          //女
 	'secrecy'                   => 'Secrecy',                       //保密
-
+	'Address_of_landlord'       => 'Address of landlord',           //房东联系地址
+	'The_landlord_note'         => 'The landlord note',             //房东备注
+	'Choose_picture'            => 'Choose picture',                //选择图片
+	'Click_here_to_upload_the_file' => 'Click here to upload the file',        //点击这里上传文件
+	'Save_and_submit_the_audit' => 'Save and submit the audit',     //保存并提交审核
+	'cancel'                    => 'Cancel',                        //取消
 ];
