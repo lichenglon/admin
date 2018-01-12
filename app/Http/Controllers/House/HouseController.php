3f84e5adc27fa8225a950400a50a7779ec9f2dce
lichenglonaa$ <34432269+lichenglon@users.noreply.github.com>
2018-01-12 18:05:17 +0800

    page

--- app/Http/Controllers/House/HouseController.php
+++ app/Http/Controllers/House/HouseController.php
@@ -467,13 +467,13 @@ class HouseController extends BaseController {
 						->where('house_type', 'like', $type)
 						->where($search_k,'like','%'.$search_v.'%')
 						->orderBy('msgid', 'desc')
-						->paginate(16);
+						->paginate(10);
 			}else{
 				$gather = DB::table('house_message')
 						->where('chk_sta','2')
 						->where('house_type', 'like', $type)
 						->orderBy('msgid', 'desc')
-						->paginate(16);
+						->paginate(10);
 			}
 
 
@@ -573,7 +573,7 @@ class HouseController extends BaseController {
 			$gather = DB::table('house_message')
 					->where('chk_sta','2')
 					->orderBy('msgid', 'desc')
-					->paginate(16);
+					->paginate(10);
 
 /*			$gather = DB::table('house_message')
 					->where('chk_sta','2')
